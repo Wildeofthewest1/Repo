fa491f6f9a618743b13f0b630be5152767dc69b9
Matt <Mattwildeusa@icloud.com>
2025-10-11 11:24:11 +0100

    2

--- testGraph.py
+++ testGraph.py
@@ -1,13 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 
-#xData = np.array([1,2,3,4,5])
-#yData = np.array([1,2,23,3,4])
+# Change the default font family and size
+rcParams['font.family'] = 'serif'        # e.g. 'sans-serif', 'monospace', etc.
+rcParams['font.serif'] = ['Times New Roman']  # specify a particular font
+rcParams['font.size'] = 14
 
-#plt.plot(xData,yData)
+# Optional: change math text font
+rcParams['mathtext.fontset'] = 'dejavuserif'  # or 'cm', 'stix', 'custom'
 
-#plt.xlabel("Transmission")
-#plt.ylabel("LinearDetuningGHz")
+points = 17
+
+yData = np.ones (points)
+xData = np.linspace (-8,8,points)
+
+print (xData)
+
+Temp = 42
+
+plt.plot(xData,yData)
+
+plt.ylabel("Transmission")
+plt.xlabel("Linear Detuning (GHz)")
+
+plt.ylim([0,1.25])
+#plt.xlim([-8.5,8.5])
+
+plt.yticks([0.00,0.25,0.50,0.75,1.00])
+plt.xticks([-8,-4,0,4,8])
 
 print("done")
 
