c1221cd13f49b06fbdb40fdf5dff4278cdd4fee5
Matt <Mattwildeusa@icloud.com>
2025-10-11 10:09:45 +0100

    First

---
+++ testGraph.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#xData = np.array([1,2,3,4,5])
+#yData = np.array([1,2,23,3,4])
+
+#plt.plot(xData,yData)
+
+#plt.xlabel("Transmission")
+#plt.ylabel("LinearDetuningGHz")
+
+print("done")
+
+plt.show()
