f7adde21b069382701b0f9c784d2dbe2eedd8bce
Matt <Mattwildeusa@icloud.com>
2025-10-15 17:51:14 +0100

    third commit

--- testGraph.py
+++ testGraph.py
@@ -1,35 +1,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from matplotlib import rcParams
 
-# Change the default font family and size
-rcParams['font.family'] = 'serif'        # e.g. 'sans-serif', 'monospace', etc.
-rcParams['font.serif'] = ['Times New Roman']  # specify a particular font
-rcParams['font.size'] = 14
+# Font setup
+fontsz = 16
 
-# Optional: change math text font
-rcParams['mathtext.fontset'] = 'dejavuserif'  # or 'cm', 'stix', 'custom'
+# Change the default font family and size 
+rcParams['font.family'] = 'serif' # e.g. 'sans-serif', 'monospace', etc.
+#rcParams['font.serif'] = ['Times New Roman'] # specify a particular font
+#rcParams['font.size'] = fontsz
+ # Optional: change math text font
+#rcParams['mathtext.fontset'] = 'dejavuserif' # or 'cm', 'stix', 'custom'
+
+############### 
+#328 nm D2 transition line
+#5s2S1/2 > 5p2P3/2
+#
+Temp = 42
+#
+# ##code for the actual plot goes here
+# 
+#
+#
+################
 
 points = 17
+yData = np.ones(points)
+xData = np.linspace(-8, 8, points)
 
-yData = np.ones (points)
-xData = np.linspace (-8,8,points)
-
-print (xData)
-
-Temp = 42
-
-plt.plot(xData,yData)
+plt.figure(figsize=(5, 3.5))
+plt.plot(xData, yData, 'k', lw=2)
 
 plt.ylabel("Transmission")
 plt.xlabel("Linear Detuning (GHz)")
 
-plt.ylim([0,1.25])
-#plt.xlim([-8.5,8.5])
+## Labels
 
-plt.yticks([0.00,0.25,0.50,0.75,1.00])
-plt.xticks([-8,-4,0,4,8])
+plt.text(x=-8, y=1.09, s="Ag-D$_2$", fontsize=fontsz+2, ha = "left", va = "center") ##Metal
+plt.text(x=8, y=1.09, s="{}$\degree$C".format(Temp), fontsize=fontsz+2, ha = "right", va = "center") ##Temperature
 
-print("done")
+plt.text(x=-8, y=0.12, s="$5^2$S$_{1/2}$", fontsize=fontsz, ha = "left", va = "center")#5s2S1/2
+plt.text(x=-8, y=0.44, s="$5^2$P$_{3/2}$", fontsize=fontsz, ha = "left", va = "center")#5p2P3/2
 
+plt.text(x=-3, y=0.27, s="D$_2$", fontsize=fontsz, ha = "left", va = "center")#5p2P3/2
+
+plt.text(x=5.5, y=0.05, s="0", fontsize=fontsz, ha = "left", va = "center")#F=0
+plt.text(x=5.5, y=0.18, s="1", fontsize=fontsz, ha = "left", va = "center")#F=1
+
+plt.text(x=5.5, y=0.37, s="1", fontsize=fontsz, ha = "left", va = "center")#F'=1
+plt.text(x=5.5, y=0.49, s="2", fontsize=fontsz, ha = "left", va = "center")#F'=2
+
+plt.text(x=6.5, y=0.12, s="$F$", fontsize=fontsz, ha = "left", va = "center")#F'=1
+plt.text(x=6.5, y=0.44, s="$F^{\prime}$", fontsize=fontsz, ha = "left", va = "center")#F'=2
+
+##
+
+plt.ylim([0, 1.2])
+plt.xlim([-8.5,8.5])
+
+plt.yticks([0.00, 0.25, 0.50, 0.75, 1.00])
+plt.xticks([-8, -4, 0, 4, 8])
+
+# --- Overlay the image ---
+img = mpimg.imread(r"C:\Users\Matt\Desktop\Lvl_4\Project\SilverD2Diagram.png")
+
+# Add image to the plot using figimage or imshow
+# Option 1: Place image in axis coordinates (0-1)
+plt.imshow(img, extent=[-5, 5, 0.05, 0.5], aspect='auto', alpha=0.7)
+
+# Option 2: Place image in figure coordinates (absolute position)
+# fig = plt.gcf()
+# fig.figimage(img, xo=400, yo=300, alpha=0.7, zorder=10)
+
+plt.savefig(r"C:\Users\Matt\Desktop\Lvl_4\Project\voigt_combined.pdf", bbox_inches='tight')
 plt.show()
